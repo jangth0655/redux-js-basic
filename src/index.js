02d6b2a77d74a5793c1d6622f8bf6387c902b561
JANGTAEHEE <jangth0655@gmail.com>
2022-01-25 01:03:56 +0900

    basic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
-import App from "./App";
+const add = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const number = document.querySelector("span");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = store.getState();
+};
+
+store.subscribe(onChange);
+
+add.addEventListener("click", () => {
+  store.dispatch({ type: ADD });
+});
+
+minus.addEventListener("click", () => {
+  store.dispatch({ type: MINUS });
+});
