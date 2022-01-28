ef57b2c054fd495ef0b1aebccc69eec0c5518bdb
JANGTAEHEE <jangth0655@gmail.com>
2022-01-28 21:35:42 +0900

    redux-basic, login app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 
-const form = document.querySelector("form");
+/* const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
@@ -66,3 +66,92 @@ const onSubmit = (e) => {
 };
 
 form.addEventListener("submit", onSubmit);
+ */
+
+const reducer = (currentState, action) => {
+  switch (
+    action.type // action === 객체
+  ) {
+    case "LOG_IN":
+      return {
+        ...currentState,
+        user: action.data,
+      };
+    case "LOG_OUT":
+      return {
+        ...currentState,
+        user: null,
+      };
+    case "ADD_POST":
+      return {
+        ...currentState,
+        posts: [...currentState.posts, action.data],
+      };
+    default:
+      return currentState;
+  }
+};
+
+const initialState = {
+  user: null,
+  posts: [],
+};
+
+const store = createStore(reducer, initialState);
+
+console.log("1st", store.getState());
+
+// actionCreator
+const login = (data) => {
+  return {
+    type: "LOG_IN",
+    data,
+  };
+};
+
+const logOut = () => {
+  return {
+    type: "LOG_OUT",
+  };
+};
+
+const addPost = (data) => {
+  return {
+    type: "ADD_POST",
+    data,
+  };
+};
+
+store.dispatch(
+  login({
+    id: 1,
+    name: "zerocho",
+    admin: true,
+  })
+);
+
+console.log("login", store.getState());
+
+store.dispatch(
+  addPost({
+    useId: 1,
+    id: 1,
+    contents: "Hello, redux",
+  })
+);
+
+console.log("addPost_1", store.getState());
+
+store.dispatch(
+  addPost({
+    useId: 2,
+    id: 2,
+    contents: "Hello, basic-redux",
+  })
+);
+
+console.log("addPost_2", store.getState());
+
+store.dispatch(logOut());
+
+console.log("logOut", store.getState());
